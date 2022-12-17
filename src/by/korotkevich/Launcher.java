84933fd8454005df49b22280887c1ac8d104f752
Vadim Korotkevich <vadimkorotkii911@gmail.com>
2022-12-17 20:56:56 +0300

    update Launcher rollback to Hello world

--- src/by/korotkevich/Launcher.java
+++ src/by/korotkevich/Launcher.java
@@ -2,6 +2,6 @@ package by.korotkevich;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Hello java");
+        System.out.println("Hello world");
     }
 }
